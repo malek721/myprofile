fed07eb19654d293284fd0e0b30fdc0d0ea55e95
M.malik elhidir <malekco721@gmail.com>
2022-04-07 17:03:01 +0300

    new

---
+++ JS/JsDeneme.js
@@ -0,0 +1,33 @@
+//window.alert("Hey");
+document.write("<h1>hello world!</h>");
+console.log("Hey");//طباعة داخل وحدة التحكم
+console.error("error");
+console.table(["muhammed","malik","elhidir"]);
+console.log("%chello %cworld","color: blue;font-size: 20px","color: red;font-size: 25px");//لتنسيق شيء في وحدة اتحكم نضيف قبل الشيء""%c ثم نكتب كزد التنسيق"
+console.log(typeof "muhammed");//string
+console.log(typeof 587); //number
+console.log(typeof undefined); //undefined
+console.log(typeof [50,79.14]); //arrey
+console.log(typeof {name: "malik",age: "20",country: "sy"});//object
+console.log(typeof true); //boolean
+console.log(typeof null); 
+
+var user = "muhamed",
+    age = "20";
+console.log(user);
+console.log(age);
+console.log(hello);
+
+console.log('Elzero Web "School"');
+console.log("Elzero Web 'School'");
+console.log("Elzero Web \"School\"");
+console.log('Elzero \\ Web \'School\'');
+console.log("Elzero \
+Web \
+School");
+console.log("Elzero\nWeb\nSchool");
+
+let a = " we love";
+let b = "JS";
+document.write( "\n" +a+" "+b);
+console.log(a, b);
